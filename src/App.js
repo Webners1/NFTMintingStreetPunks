96ca4d96ce3ccdad1eaca1cb39652b4a64ca958d
Webners1 <Webners1>
2022-03-06 21:10:52 +0500

    mintingDapp

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .mint(totalCostWei, blockchain.account,mintAmount)
+      .mint(blockchain.account,mintAmount)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
